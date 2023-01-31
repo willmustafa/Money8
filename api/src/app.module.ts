25fe5479391deeec02f3e14064902fd7ac4f641e
willian mustafa <willian@integra.do>
2023-01-31 13:08:26 -0300

    merge fix

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
 
