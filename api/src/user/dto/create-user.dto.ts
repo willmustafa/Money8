3b989711a7ff974c5e9fc1a2e31d1f0315aa36a0
willian mustafa <willian@integra.do>
2023-01-31 13:40:18 -0300

    #14 - Adiciona o CRUD de user

--- api/src/user/dto/create-user.dto.ts
+++ api/src/user/dto/create-user.dto.ts
@@ -1,1 +1,7 @@
-export class CreateUserDto {}
+import { Prisma } from '@prisma/client';
+
+export class CreateUserDto implements Prisma.UserCreateInput {
+  fullName: string;
+  email: string;
+  password: string;
+}
--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -8,22 +8,31 @@ export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.prisma.user.create({
+      data: {
+        ...createUserDto,
+      },
+    });
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.prisma.user.findMany({});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.prisma.user.findUnique({ where: { id } });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.prisma.user.update({
+      where: { id },
+      data: updateUserDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    return this.prisma.user.delete({
+      where: { id },
+    });
   }
 }
