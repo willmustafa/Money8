6910812a2d190861ead7466e51306337191c0177
willian mustafa <willian@integra.do>
2023-01-31 14:06:51 -0300

    #14 - Adiciona brypt

--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -2,28 +2,46 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { genSalt, hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
-  create(createUserDto: CreateUserDto) {
+  private readonly SALT_GEN_ROUNDS: number = 10;
+
+  async create(createUserDto: CreateUserDto) {
+    const data = (await this.generateHash(createUserDto)) as CreateUserDto;
+
     return this.prisma.user.create({
-      data: {
-        ...createUserDto,
-      },
+      data,
     });
   }
 
   findAll() {
-    return this.prisma.user.findMany({});
+    return this.prisma.user.findMany({
+      select: {
+        id: true,
+        email: true,
+        fullName: true,
+      },
+    });
   }
 
   findOne(id: string) {
-    return this.prisma.user.findUnique({ where: { id } });
+    return this.prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        email: true,
+        fullName: true,
+      },
+    });
   }
 
-  update(id: string, updateUserDto: UpdateUserDto) {
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    updateUserDto = await this.generateHash(updateUserDto);
+
     return this.prisma.user.update({
       where: { id },
       data: updateUserDto,
@@ -34,5 +52,15 @@ export class UserService {
     return this.prisma.user.delete({
       where: { id },
     });
+  }
+
+  async generateHash(
+    dto: CreateUserDto | UpdateUserDto,
+  ): Promise<CreateUserDto | UpdateUserDto> {
+    if (dto.password) {
+      const salt = await genSalt(this.SALT_GEN_ROUNDS);
+      dto.password = await hash(dto.password, salt);
+    }
+    return dto;
   }
 }
