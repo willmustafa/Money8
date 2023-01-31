4c87743a1bfc7d25fda6d52095d58792e03139f5
willian mustafa <willian@integra.do>
2023-01-31 13:05:59 -0300

    #14 - User Service init

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [],
   providers: [PrismaService],
 })
