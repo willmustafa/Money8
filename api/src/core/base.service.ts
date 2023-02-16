ecb32d6880eac0e1100896a035fa190114b022fa
willian mustafa <willian@integra.do>
2023-02-15 21:57:19 -0300

    #41: Adiciona o query params e filtra todas as requisições pelo userId

--- api/src/core/base.service.ts
+++ api/src/core/base.service.ts
@@ -1,3 +1,5 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
 export abstract class BaseService {
   constructor(private readonly prismaModel: any) {}
 
@@ -9,17 +11,33 @@ export abstract class BaseService {
     });
   }
 
-  findAll(): any {
-    return this.prismaModel.findMany({});
+  async findAll(page = 1, pageSize = 25, sort = {}, filter = {}): Promise<any> {
+    const items = await this.prismaModel.findMany({
+      take: pageSize,
+      skip: (page - 1) * pageSize,
+      where: filter,
+      orderBy: sort,
+    });
+
+    const totalCount = await this.prismaModel.count({ where: filter });
+
+    return {
+      page: Number(page),
+      pageSize: Number(pageSize),
+      totalCount: Number(totalCount),
+      items,
+    };
   }
 
-  findOne(id: string): any {
-    return this.prismaModel.findUnique({
-      where: { id },
+  findOne(id: string, filter = {}): any {
+    return this.prismaModel.findFirst({
+      where: { id, ...filter },
     });
   }
 
-  update(id: string, updateDto: any): any {
+  async update(id: string, updateDto: any, filter: any = {}): Promise<any> {
+    await this.failIfNotFromSameUser(id, filter);
+
     return this.prismaModel.update({
       where: { id },
       data: {
@@ -28,9 +46,17 @@ export abstract class BaseService {
     });
   }
 
-  remove(id: string): any {
+  async remove(id: string, filter = {}): Promise<any> {
+    await this.failIfNotFromSameUser(id, filter);
+
     return this.prismaModel.delete({
       where: { id },
     });
+  }
+
+  private async failIfNotFromSameUser(id: string, filter: any = {}) {
+    const entityOnDb = await this.findOne(id, filter);
+    if (entityOnDb.userId !== filter.userId)
+      throw new HttpException('Not found', HttpStatus.NOT_FOUND);
   }
 }
