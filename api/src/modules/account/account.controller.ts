1cc67fdd170b305e64543bc9f6de4338bff50232
willian mustafa <willian@integra.do>
2023-02-26 18:54:35 -0300

    #45 : Adiciona a soma no account

--- api/src/modules/account/account.controller.ts
+++ api/src/modules/account/account.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Request } from '@nestjs/common';
 import { BaseController } from 'src/core/base.controller';
 import { AccountService } from './account.service';
 
@@ -6,5 +6,14 @@ import { AccountService } from './account.service';
 export class AccountController extends BaseController {
   constructor(private readonly accountService: AccountService) {
     super(accountService);
+  }
+
+  @Get('sum')
+  async getMonthlySum(
+    @Query('date') date = new Date().toISOString(),
+    @Request() request: any,
+  ) {
+    this.accountService.userId = request.user.id;
+    return await this.accountService.getMonthlySum(date);
   }
 }
