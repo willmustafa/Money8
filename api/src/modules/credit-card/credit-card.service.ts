51024a0895f43dd8b03f6951809ebbee4e66c2f9
willian mustafa <willian@integra.do>
2023-02-27 20:24:21 -0300

    #46: Adiciona o filtro para adicionar a soma de transações

--- api/src/modules/credit-card/credit-card.service.ts
+++ api/src/modules/credit-card/credit-card.service.ts
@@ -19,4 +19,50 @@ export class CreditCardService extends BaseService {
       },
     });
   }
+
+  async findAll(
+    page = 1,
+    pageSize = 25,
+    sort = {},
+    filter = {} as any,
+  ): Promise<any> {
+    let result = await super.findAll(page, pageSize, sort, {});
+
+    if (filter?.show_sum === 'true')
+      result = await this.appendSum(result, filter);
+
+    return result;
+  }
+
+  async appendSum(result: any, filter: any) {
+    const sumBy = await this.prisma.transaction.groupBy({
+      where: {
+        userId: this.userId,
+        date: {
+          lte: filter?.date ?? new Date().toISOString(),
+        },
+        paid: true,
+        fromCreditCard: {
+          not: null,
+        },
+      },
+      by: ['fromCreditCard'],
+      _sum: {
+        value: true,
+      },
+      orderBy: {
+        _sum: {
+          value: 'desc',
+        },
+      },
+    });
+
+    result.items = result.items.map((acc: any) => {
+      const sum =
+        sumBy.find((el) => el.fromCreditCard === acc.id)?._sum.value ?? 0;
+      return { ...acc, sum };
+    });
+
+    return result;
+  }
 }
