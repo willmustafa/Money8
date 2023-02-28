54f6888cf9a9a460e5d53297c9caa1fc2037c81d
willian mustafa <willian@integra.do>
2023-02-27 22:22:06 -0300

    #43: Adiciona a somatoria do mes e total de transações

--- api/src/modules/transaction/transaction.controller.ts
+++ api/src/modules/transaction/transaction.controller.ts
@@ -1,4 +1,5 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Request } from '@nestjs/common';
+import { parse } from 'qs';
 import { BaseController } from 'src/core/base.controller';
 import { TransactionService } from './transaction.service';
 
@@ -6,5 +7,13 @@ import { TransactionService } from './transaction.service';
 export class TransactionController extends BaseController {
   constructor(private readonly transactionService: TransactionService) {
     super(transactionService);
+  }
+
+  @Get('sum')
+  async getMonthlySum(@Query('filter') filter = '', @Request() request: any) {
+    const parsedFilter = filter === '' ? {} : parse(filter);
+    this.transactionService.userId = request.user.id;
+
+    return await this.transactionService.getMonthlySum(parsedFilter);
   }
 }
--- api/src/modules/transaction/transaction.service.ts
+++ api/src/modules/transaction/transaction.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { BaseService } from 'src/core/base.service';
+import {
+  getFirstDayOfMonth,
+  getLastDayOfMonth,
+  subtractOneMonth,
+} from 'src/shared/helpers/Dates';
 import { PrismaService } from 'src/shared/prisma/prisma.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 
@@ -17,5 +22,106 @@ export class TransactionService extends BaseService {
         ...createTransactionDto,
       },
     });
+  }
+
+  async getMonthlySum(filters: any) {
+    const expenses = (await this.getExpensesByMonth(filters?.date)) ?? 0;
+    const expensesFromLastMonth =
+      (await this.getExpensesByMonth(subtractOneMonth(filters?.date))) ?? 0;
+
+    const incomes = (await this.getIncomesByMonth(filters?.date)) ?? 0;
+    const incomesFromLastMonth =
+      (await this.getIncomesByMonth(subtractOneMonth(filters?.date))) ?? 0;
+
+    const balance = incomes - Math.abs(expenses);
+    const totalBalance = (await this.getTotalSum()) ?? 0;
+    return {
+      expenses,
+      expensesFromLastMonth,
+      expensesVariation: this.getPercentage(expenses, expensesFromLastMonth),
+      incomes,
+      incomesFromLastMonth,
+      incomesVariation: this.getPercentage(incomes, incomesFromLastMonth),
+      balance,
+      totalBalance,
+    };
+  }
+
+  async getExpensesByMonth(date: string) {
+    return await this.prisma.transaction
+      .aggregate({
+        where: {
+          userId: this.userId,
+          date: {
+            lte: getLastDayOfMonth(date),
+            gte: getFirstDayOfMonth(date),
+          },
+          paid: true,
+          value: {
+            lte: 0,
+          },
+        },
+        _sum: {
+          value: true,
+        },
+      })
+      .then((res) => res._sum.value);
+  }
+
+  async getIncomesByMonth(date: string) {
+    return await this.prisma.transaction
+      .aggregate({
+        where: {
+          userId: this.userId,
+          date: {
+            lte: getLastDayOfMonth(date),
+            gte: getFirstDayOfMonth(date),
+          },
+          paid: true,
+          value: {
+            gte: 0,
+          },
+        },
+        _sum: {
+          value: true,
+        },
+      })
+      .then((res) => res._sum.value);
+  }
+
+  async getTotalSum() {
+    const transactionSum =
+      (await this.prisma.transaction
+        .aggregate({
+          where: {
+            userId: this.userId,
+            paid: true,
+          },
+          _sum: {
+            value: true,
+          },
+        })
+        .then((res) => res._sum.value)) ?? 0;
+
+    const accountSum =
+      (await this.prisma.account
+        .aggregate({
+          where: {
+            userId: this.userId,
+          },
+          _sum: {
+            initialBalance: true,
+          },
+        })
+        .then((res) => res._sum.initialBalance)) ?? 0;
+
+    return accountSum + transactionSum;
+  }
+
+  getPercentage(current: number, last: number): number {
+    current = Math.abs(current);
+    last = Math.abs(last);
+    const divided = last === 0 ? 1 : last;
+    return ((current - last) / divided) * 100;
   }
 }
