845e68c59e5adfaf90be588b803e6f610aae24e7
willian mustafa <willian@integra.do>
2023-04-18 14:02:57 -0300

    Adiciona card de cartão

---
+++ front/src/layouts/core/components/CreditCardCard/CreditCardCard.tsx
@@ -0,0 +1,64 @@
+import React, { MouseEventHandler } from "react";
+import styles from "./CreditCardCard.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import CustomCard from "../UI/customCard/customCard";
+import { toCurrency } from "@/helpers/Numbers";
+
+interface ICustomCard {
+  bg?: "plus" | "generic";
+  title: string;
+  icon?: React.ReactNode;
+  currentValue: number;
+  limit?: number;
+  paymentDate?: string;
+  handleEdit?: MouseEventHandler;
+  handleImport?: MouseEventHandler;
+  handleCreate?: MouseEventHandler;
+}
+
+export default function CreditCardCard(props: ICustomCard) {
+  return (
+    <CustomCard {...props}>
+      <div className={`${styles.customCardInner} ${styles.expansive}`}>
+        <div className={styles.cardHeader}>
+          <span>Fatura atual</span>
+          <div className={`d-flex gap-3 ${styles.iconWrapper}`}>
+            <span
+              className={styles.click}
+              onClick={props.handleEdit ?? function () {}}
+            >
+              <FontAwesomeIcon icon={["fas", "edit"]} />
+            </span>
+            <span
+              className={styles.click}
+              onClick={props.handleImport ?? function () {}}
+            >
+              <FontAwesomeIcon icon={["fas", "cloud-arrow-up"]} />
+            </span>
+          </div>
+        </div>
+        <div className={styles.cardBody}>
+          <div className="d-block">
+            <h4 className="text-white fw-bold">
+              {toCurrency(props.currentValue)}
+            </h4>
+            {props.limit && (
+              <p className={`mb-0 ${styles.subtext}`}>
+                Limite: {toCurrency(props.limit)}
+              </p>
+            )}
+          </div>
+          <div className={`mt-auto ${styles.cardFooter}`}>
+            <div className={styles.logoWrapper}>{props.icon}</div>
+            {props.paymentDate && (
+              <div className={styles.paymentDate}>
+                <p className="mb-0">Data de pagamento:</p>
+                <p className="mb-0">{props.paymentDate}</p>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </CustomCard>
+  );
+}
--- front/src/pages/cartoes.tsx
+++ front/src/pages/cartoes.tsx
@@ -1,24 +1,42 @@
+import CreditCardCard from "@/layouts/core/components/CreditCardCard/CreditCardCard";
 import CustomCard from "@/layouts/core/components/UI/customCard/customCard";
 import Image from "next/image";
 import React from "react";
 
 export default function Contas() {
-  const accountEditHandler = () => {alert('edit')}
-  const accountImportHandler = () => {alert('import')}
-  const accountCreateHandler = () => {alert('create')}
+  const accountEditHandler = () => {
+    alert("edit");
+  };
+  const accountImportHandler = () => {
+    alert("import");
+  };
+  const accountCreateHandler = () => {
+    alert("create");
+  };
 
   return (
     <div className="row">
-      <div className="col-3">
-        <CustomCard title="Criar novo cartão" handleCreate={accountCreateHandler} />
-      </div>
-      <div className="col-3">
+      <div className="col-4">
         <CustomCard
-            bg="generic"
-          title="Fatura Atual"
-          creditCard
-          text="R$ 35.000,00"
-          subtext="12%"
+          title="Criar novo cartão"
+          handleCreate={accountCreateHandler}
+        />
+      </div>
+      <div className="col-4">
+        <CreditCardCard
+          bg="generic"
+          title="NuBank"
+          icon={
+            <Image
+              src="/institutions/nubank.svg"
+              alt="bank logo"
+              width={55}
+              height={30}
+            />
+          }
+          currentValue={600}
+          limit={1200}
+          paymentDate="08/20"
           handleEdit={accountEditHandler}
           handleImport={accountImportHandler}
         />
